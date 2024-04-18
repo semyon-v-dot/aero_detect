8a90d8353c603c43243bd6250f2de05f0ea4df92
semyon-v-dot <semyon-v-dot@yandex.ru>
2024-04-18 13:46:26 +0500

    Add "save_txt" method

--- vid_test.py
+++ vid_test.py
@@ -9,15 +9,14 @@ if __name__ == '__main__':
     vid_fps = vid.get(cv.CAP_PROP_FPS)
     vid.set(1, vid_fps * SKIP_SEC_BEGINNING)
     ret, frame1 = vid.read()
-    ret, frame2 = vid.read()
-    fr_counter = 2
+    fr_counter = 0
 
-    model = YOLO("aero2.pt")
+    model = YOLO("aero1.pt")
 
     fourcc = cv.VideoWriter_fourcc(*'mp4v')
-    out_name = "IMG_1855_PREDICTED_AERO2.MP4"
-    out = cv.VideoWriter(out_name, fourcc, 30.0, (1280, 720))
-    while ret and vid.isOpened():
+    out_name = "IMG_1855_PREDICTED_AERO2_TEST.MP4"
+    # out = cv.VideoWriter(out_name, fourcc, 30.0, (1280, 720))
+    while True:
         # res = model(cv.cvtColor(frame1, cv.COLOR_BGR2RGB), size=640)
         results = model.predict(source=frame1)
         for res in results:
@@ -30,19 +29,24 @@ if __name__ == '__main__':
                     (255),
                     thickness=1
                     )
+                
+                res.save_txt(f"./ann/frame_{str(fr_counter).zfill(6)}.txt")
         
-        out.write(frame1)
+        # out.write(frame1)
             
         cv.imshow("", frame1)
         waitkey = cv.waitKey(1)
         if waitkey == ord('q'):
             break
 
-        frame1 = frame2
         ret, frame2 = vid.read()
+        frame1 = frame2
         fr_counter += 1
         sec = fr_counter / int(vid_fps)
 
-    out.release()
+        if not ret or not vid.isOpened():
+            break
+
+    # out.release()
     vid.release()
     cv.destroyAllWindows()
